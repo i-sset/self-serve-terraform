46b537ec331bc82e87bc6df63782f435e396be74
i-sset <isset.josset@gmail.com>
2023-02-03 17:56:55 -0300

    fix: add missing rt_association

--- route_table_and_associations.tf
+++ route_table_and_associations.tf
@@ -11,3 +11,8 @@ resource "aws_route_table_association" "rt_association_1" {
   subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.main_rt.id
 }
+
+resource "aws_route_table_association" "rt_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.main_rt.id
+}
